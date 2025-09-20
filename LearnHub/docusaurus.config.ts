2b8567e3ca82a96717798d9020da9d8a53c96eca
Mungara-frency <frencyadam9493@gmail.com>
2025-09-20 09:57:46 +0530

    Add files via upload

---
+++ LearnHub/docusaurus.config.ts
@@ -0,0 +1,169 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'LearnHub',
+  tagline: 'LearnHub is a platform for learning web development and AI',
+  favicon: 'img/learnhub.png',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'souvikpramanikgit', // Usually your GitHub org/user name.
+  projectName: 'learnhub', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
+    navbar: {
+      hideOnScroll: true,
+      title: 'LearnHub',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/learnhub.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          to: '/blog',
+          label: 'Blog',
+          position: 'left'
+        },
+        {
+          type: 'dropdown',
+          label: 'More',
+          position: 'right',
+          items: [
+            {
+              type: 'docSidebar',
+              sidebarId: 'notesSidebar',
+              label: 'Notes',
+            },
+            { 
+              to: '/news', 
+              label: 'Tech News'
+            },
+            { 
+              to: '/lectures', 
+              label: 'Video Lectures'
+            },
+            {
+              label: 'Code Playground',
+              to: '/docs/code-playground-demo',
+            },
+          ],
+        },
+        {
+          href: 'https://github.com/souvikpramanikgit/LearnHub',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    // footer: {
+    //   style: 'dark',
+    //   links: [
+    //     {
+    //       title: 'Docs',
+    //       items: [
+    //         {
+    //           label: 'Tutorial',
+    //           to: '/docs/intro',
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: 'Community',
+    //       items: [
+    //         { 
+    //           label: 'Discord', 
+    //           href: 'https://discord.gg/YmSKSDfH' 
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: 'More',
+    //       items: [
+    //         {
+    //           label: 'Blog',
+    //           to: '/blog',
+    //         },
+    //         {
+    //           label: 'GitHub',
+    //           href: 'https://github.com/souvikpramanikgit/LearnHub',
+    //         },
+    //       ],
+    //     },
+    //   ],
+
+    //   copyright: `Copyright © ${new Date().getFullYear()} LearnHub. Built with Docusaurus.`,
+    // },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
